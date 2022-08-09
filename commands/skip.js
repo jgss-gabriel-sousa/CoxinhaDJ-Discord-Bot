bf9d6890e488a05e7ddce9a4678f74dad58f0512
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2022-08-09 18:10:36 -0300

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -4,6 +4,11 @@ module.exports = {
     name: "skip",
     description: "Deixar de tocar musga",
     async execute(message, args){
-        getVoiceConnection(message.guild.id).disconnect();
+        try{
+            getVoiceConnection(message.guild.id).disconnect();
+        }
+        catch(error){
+            console.log(error);
+        }
     }
 }
