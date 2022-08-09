1e76d89545dd02c23c45beca4ba049591f668cd3
Gabriel Sousa <jgss.gabriel.sousa@gmail.com>
2022-08-09 18:10:31 -0300

    Update shot.js

--- commands/shot.js
+++ commands/shot.js
@@ -1,14 +1,59 @@
+const bebidas = [
+    {
+        name: "Pitu",
+        alcool: 40
+    },
+    {
+        name: "Cerveja",
+        alcool: 5
+    },
+    {
+        name: "Vinho",
+        alcool: 14
+    },
+    {
+        name: "Tequila",
+        alcool: 27
+    },
+    {
+        name: "Vodka",
+        alcool: 40
+    },
+    {
+        name: "Whisky",
+        alcool: 54
+    },
+    {
+        name: "Caipirinha",
+        alcool: 48
+    },
+    {
+        name: "Gin",
+        alcool: 50
+    },
+    {
+        name: "Alcool 70",
+        alcool: 70
+    },
+]
+
 module.exports = {
     name: "shot",
     description: "Shotzin de bebida. Tá com o fígado em dia???",
     execute(message, args){
-        const result = Math.random() * (100 - 0) + 0;
+        const result = Math.floor(Math.random() * (100 - 0) + 0);
+        const shots = Math.floor(Math.random() * (5 - 0) + 1);
+        const bebida = bebidas[Math.floor(Math.random() * (bebidas.length - 0) + 0)];
+        const embriaguez = bebida.alcool * shots;
 
-        if(result >= 70){
-            message.channel.send("Ficou bêbado");
+        if(embriaguez > 100){
+            message.reply(`Tomou ${shots} shot de ${bebida.name} e está em coma alcoólico`);
+        }
+        else if(result >= embriaguez){
+            message.reply(`Tomou ${shots} shot de ${bebida.name} e está bêbado`);
         }
         else{
-            message.channel.send("Está sóbrio");
+            message.reply(`Tomou ${shots} shot de ${bebida.name} e está sóbrio`);
         }
     }
 }
